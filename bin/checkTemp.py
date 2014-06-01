20246cf344bf48ccdb51d93d11c62338dace29ee
BiocomSoftware <Mike.Rightmire@BiocomSoftware.com>
2014-06-01 23:35:25 +0200

    New

--- bin/checkTemp.py
+++ bin/checkTemp.py
@@ -5,18 +5,21 @@ import subprocess
 import time
 import datetime
 
-logfile = open('/var/log/temp.log', 'a', 0)
+# logfile = open('/var/log/temp.log', 'a', 0)
 
-loop = True
-while loop:
+def getSensors():
     proc = subprocess.Popen('sensors',stdout=subprocess.PIPE)
     sense = proc.communicate()[0]
     sense = StringIO.StringIO(sense)
-    print >>logfile, "----------"
-    print >>logfile,  str(datetime.datetime.now().strftime('%Y-%m-%d:%Mmin.%Ssec'))
+    result = []
+    
     for line in sense.readlines():
         line = ''.join(c for c in line if c not in "\n\r")
-        if (("C" in line) and ('PCI' not in line)):
-            print >>logfile, line
-    time.sleep(15)        
-#    loop = False
+#         line = line.encode('ascii')
+        result.append(line)
+    
+    return result
+
+    
+if __name__ == "__main__":
+    print getSensors()
